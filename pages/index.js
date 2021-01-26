c5107bb6b18916737f94b393365ce1b178665b74
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-25 23:36:52 -0300

    feat: add Home Components

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,36 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import QuizBackground from '../src/components/QuizBackground'
+import Footer from '../src/components/Footer'
+import GithubCorner from '../src/components/GithubCorner'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Quiz CSS da Alura</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Hmm</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GithubCorner projectUrl="https://github.com/lucaswilliameufrasio/AluraQuiz" />
+    </QuizBackground>
+  )
 }
