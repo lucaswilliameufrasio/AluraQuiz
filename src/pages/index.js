2a3297e656bee57ac08bebac82df8665cba2cff3
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-27 03:44:48 -0300

    refactor: move QuizContainer Components to its own file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,11 @@
-import styled from 'styled-components'
 import db from '../../db.json'
 import Widget from '../components/Widget'
+import QuizContainer from '../components/QuizContainer'
 import QuizBackground from '../components/QuizBackground'
 import Footer from '../components/Footer'
 import GithubCorner from '../components/GithubCorner'
 
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`
+
 
 export default function Home() {
   return (
