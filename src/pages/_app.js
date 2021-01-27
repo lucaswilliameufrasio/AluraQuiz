1001cecf33fbd7de5fd4a435592f4a9a5f769c95
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-27 03:48:28 -0300

    feat: add Lato font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -40,6 +40,11 @@ export default function App({ Component, pageProps }) {
         <meta property='og:image:type' content='image/jpeg' />
         <meta property='og:image:width' content='800' />
         <meta property='og:image:height' content='600' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap'
+          rel='stylesheet'
+        />
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
