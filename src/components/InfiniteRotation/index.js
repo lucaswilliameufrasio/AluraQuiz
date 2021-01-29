deaff69edf89762ab0951369cd82e39dd904d20a
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-29 03:20:47 -0300

    feat: add InfiniteRotation Component

---
+++ src/components/InfiniteRotation/index.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+const InfiniteRotation = styled.div`
+  & {
+    -webkit-animation: spin 4s linear infinite;
+    -moz-animation: spin 4s linear infinite;
+    animation: spin 4s linear infinite;
+  }
+  @-moz-keyframes spin {
+    100% {
+      -moz-transform: rotate(360deg);
+    }
+  }
+  @-webkit-keyframes spin {
+    100% {
+      -webkit-transform: rotate(360deg);
+    }
+  }
+  @keyframes spin {
+    100% {
+      -webkit-transform: rotate(360deg);
+      transform: rotate(360deg);
+    }
+  }
+`
+
+export default InfiniteRotation
